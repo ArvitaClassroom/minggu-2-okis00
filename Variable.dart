58561c90a5d0ae75b1fa02a48dcdcbdfa195c6d6
XbalTab <iqbalhikamsnp123@gmail.com>
2024-02-07 20:32:21 +0700

    minggu-2
    nyicil

---
+++ Variable.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = "oki"; // Tipe
+  var angka = 20;
+  var todayIsFriday = false;
+  print(name); // "oki"
+  print(angka); // 20
+  print(todayIsFriday); // false
+}
---
+++ login.dart
@@ -0,0 +1,5 @@
+import 'dart:io';
+void main(List<String> args){
+ print("masukan password");
+ String inputText = stdin.readLineSync()!;
+ print("password: ${inputText}");
